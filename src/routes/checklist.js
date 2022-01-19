cc7010a9cec7d6c817c72cc3e54bfbe06d338214
Diego <diegorf55@gmail.com>
2022-01-19 01:12:18 -0300

    criando rota GET by ID

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const router = express.Router();
 
@@ -11,6 +12,11 @@ router.get('/checklists', (req, res) => {
 router.post('/', (req, res) => {
     console.log(req.body);
     res.status(200).send(req.body)
+})
+
+router.get('/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`ID: ${req.params.id}`)
 })
 
 module.exports = router;
