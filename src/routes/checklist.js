35e73a4737f89df34d0ebf0c6069d1b32010c18b
Diego <diegorf55@gmail.com>
2022-01-19 01:16:05 -0300

    criado rotas PUT e DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -19,4 +19,15 @@ router.get('/:id', (req, res) => {
     res.send(`ID: ${req.params.id}`)
 })
 
+
+router.put('/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`DELETE ID: ${req.params.id}`)
+})
+
 module.exports = router;
