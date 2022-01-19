0f0d35a6c456fd32e690a6c9d2d8a11b42d7c019
Diego <diegorf55@gmail.com>
2022-01-19 00:55:38 -0300

    concentrando rotas

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 //root do projeto
 const express = require('express');
-
+const checklistRouter = require('./src/routes/checklist')
 const app = express();
 
 //Middlewar (deixa o json disponível no req)
 app.use(express.json());
 
-//retorna dados json da requisição
-const log = (req, res, next) => {
-    console.log(req.body);
-    console.log(`Data: ${Date.now()}`)
-    next();
-}
+app.use('/checklists',checklistRouter)
 
-//usando Middlewares
-app.use(log);
-
-app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas Mágicas</h1>')
-});
-
-app.get('/json', (req, res) => {
-   res.json({title: 'Tarefa X', done: true}) 
-})
 
 app.listen(3000, () => {
     console.log('Servidor iniciado')
