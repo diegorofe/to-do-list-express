68942b165d0e432ea536430610953680d6651764
Diego <diegorf55@gmail.com>
2022-01-19 00:23:30 -0300

    criando rota que devolve um json

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ app.get('/', (req, res) => {
     res.send('<h1>Minha lista de tarefas Mágicas</h1>')
 });
 
+app.get('/json', (req, res) => {
+   res.json({title: 'Tarefa X', done: true}) 
+})
+
 app.listen(3000, () => {
     console.log('Servidor iniciado')
 });
 
-//servidor provsorio para monitorar os arquivos
+//servidor provisorio para monitorar os arquivos
