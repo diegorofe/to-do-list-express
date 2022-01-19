553422b22a29fec9aaa7afe1dfbac55b00eecbc4
Diego <diegorf55@gmail.com>
2022-01-19 00:19:38 -0300

    criando a primeira rota(GET) com o express

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 //root do projeto
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Minha lista de tarefas Mágicas</h1>')
+});
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado')
+});
+
+//servidor provsorio para monitorar os arquivos
